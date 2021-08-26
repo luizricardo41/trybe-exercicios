9fd8a82c50aeb2c5459fd034d8f40c2142d0589e
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-26 15:29:08 -0300

    Modificando arquivo

---
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-02-javascript-array-for/arrays/scripts.js
@@ -0,0 +1,9 @@
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+let searchForFirstTask = tasksList[0];
+console.log(searchForFirstTask);
+// Tomar café
+
+let searchForLastTask2 = tasksList[tasksList.length - 1];
+console.log(searchForLastTask2);
+// Brincar com o cachorro
