932c1e0df095bb3b91dc827f15bc293fc9d967b5
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-26 11:02:29 -0300

    Exercicio de variaveis

---
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/variaveis/scripts.js
@@ -0,0 +1,9 @@
+const myName = "Luiz Ricardo";
+const birthCity = "Itapeva-SP";
+let birthYear = 1989;
+birthYear = 2030;
+birthCity = "Sorocaba"; //Não é possivel alterar a variavel devido a mesma ser uma constante.
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
