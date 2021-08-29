1ec4089be407708e3033148ff117cae2f4b5a987
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-29 20:36:45 -0300

    Resolução do exercicio 9

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
@@ -168,11 +168,23 @@
 
 //Exercicio 8 - Programa para verificar se apenas 1 número de 3 seja par (retornar true).
 
-const a = 11;
-const b = 3;
-const c = 21;
+//const a = 11;
+//const b = 3;
+//const c = 21;
+//
+//if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
+//  console.log (true);
+//} else {
+//  console.log (false);
+//}
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0){
+//Exercicio 9 - Programa para verificar se apenas 1 número de 3 seja ímpar (retornar true).
+
+const a = 10;
+const b = 4;
+const c = 20;
+
+if (a % 2 !== 0 || b % 2 !== 0 || c % 2 !== 0){
   console.log (true);
 } else {
   console.log (false);
