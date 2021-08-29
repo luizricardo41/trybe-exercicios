669a7f909448078da65929b9cfc901bb03cbe590
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-29 15:12:55 -0300

    Resolução do exercicio 1

---
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
@@ -0,0 +1,34 @@
+// Exercicio 1 - Fazer 5 programas, um para cada operação aritmética:
+
+const a = 32;
+const b = 8;
+
+//adição
+let soma;
+
+soma = (a+b);
+console.log(soma);
+
+//subtração
+let diferença;
+
+diferença = (a-b);
+console.log(diferença);
+
+//divisão
+let quociente;
+
+quociente = (a/b);
+console.log(quociente);
+
+//multiplicação
+let produto;
+
+produto = (a*b);
+console.log(produto);
+
+//modulo
+let resto;
+
+resto = (a%b);
+console.log(resto);
