50af04bd3209c722ba2b18f15f626cd726c1381e
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-29 15:45:11 -0300

    Resolução do exercicio 5

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
@@ -66,12 +66,31 @@
 
 //Exercicio 4 - Verificar se o valor da constante é positivo, negativo ou falso.
 
-const a = 0;
+//const a = 0;
+//
+//if (a > 0) {
+//  console.log ("Positive");
+//} else if (a < 0) {
+//  console.log ("Negative");
+//} else {
+//  console.log ("Zero");
+//}
+//
 
-if (a > 0) {
-  console.log ("Positive");
-} else if (a < 0) {
-  console.log ("Negative");
+//Exercicio 5 - Verificar se os 3 valores da constante representam a de um triangulo (retornar true ou false).
+
+const a = -50;
+const b = 40;
+const c = 90;
+
+if (a > 0 && b > 0 && c > 0){
+
+  if (a + b + c === 180){
+    console.log (true);
+  } else {
+    console.log(false);
+  }
+
 } else {
-  console.log ("Zero");
+  console.log ("Erro: Um dos valores apresentados não é válido");
 }
