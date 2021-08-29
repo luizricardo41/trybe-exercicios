ba6108b5942cc7db32af2638febbd9a79f531b9b
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-29 15:28:12 -0300

    Resolução do exercicio 3

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
@@ -33,15 +33,32 @@
 //resto = (a%b);
 //console.log(resto);
 
+
 //Exercicio 2 - Programa que retorna o maior entre dois números:
 
-const a = 15;
-const b = 20;
+//const a = 15;
+//const b = 20;
+//let resultado;
+//
+//if(a > b) {
+//  resultado = a;
+//} else{
+//  resultado = b;
+//}
+//console.log(resultado);
+
+
+//Exercicio 3 - Programa que retorna o maior entre três números:
+const a = 21;
+const b = 19;
+const c = 20;
 let resultado;
 
-if(a > b) {
+if(a > b && a > c){
   resultado = a;
-} else{
-  resultado = b;
+} else if( b > a && b > c){
+  resultado= b;
+} else {
+  resultado = c;
 }
-console.log(resultado);
+console.log (resultado);
