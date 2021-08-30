1fc28e0d843a79c746487be0a30c7f28c9692b16
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-29 22:04:01 -0300

    Resolução do exercicio 11

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-01-javascript-primeiros-passos/exercicios/scripts.js
@@ -192,16 +192,59 @@
 
 //Exercicio 10 - Programa para calcular o lucro de venda de um produto.
 
-const custo = 130;
-const venda = 320;
-let lucro;
-let custoTotal;
+//const custo = 130;
+//const venda = 320;
+//let lucro;
+//let custoTotal;
+//
+//custoTotal = custo + (custo * 0.20);
+//
+//if (custo <= 0 || venda <= 0) {
+//  console.log ("Os valores não podem ser iguais ou menores que zero!")
+//} else {
+//  lucro = (1000 * venda) - (1000 * custoTotal);
+//}
+//console.log ("O lucro será de: " + lucro);
 
-custoTotal = custo + (custo * 0.20);
+//Exercicio 11 - Programa para calcular o salario liquido de acordo com descontos dos impostos para cada faixa de salário.
 
-if (custo <= 0 || venda <= 0) {
-  console.log ("Os valores não podem ser iguais ou menores que zero!")
-} else {
-  lucro = (1000 * venda) - (1000 * custoTotal);
+const salarioBruto = 10000.30;
+let salarioLiquido;
+let aliquotaINSS;
+let aliquotaIR;
+
+if (salarioBruto <= 1556.94) {
+  aliquotaINSS = salarioBruto * 0.08;
+  salarioLiquido = salarioBruto - aliquotaINSS;
+} 
+else if (salarioBruto <= 2594.92){
+  aliquotaINSS = salarioBruto * 0.09;
+  salarioLiquido = salarioBruto - aliquotaINSS;
 }
-console.log ("O lucro será de: " + lucro);
+else if (salarioBruto <= 5189.82){
+  aliquotaINSS = salarioBruto * 0.11;
+  salarioLiquido = salarioBruto - aliquotaINSS;
+}
+else {
+  aliquotaINSS = 570.88;
+  salarioLiquido = salarioBruto - aliquotaINSS;
+}
+
+if (salarioLiquido <= 1903.98) {
+  aliquotaIR = 0;
+}
+else if (salarioLiquido <= 2826.65) {
+  aliquotaIR = (salarioLiquido * 0.075) - 142.80;
+}
+else if (salarioLiquido <= 3751.05) {
+  aliquotaIR = (salarioLiquido * 0.15) - 354,80;
+}
+else if (salarioLiquido <= 4664.68) {
+  aliquotaIR = (salarioLiquido * 0.225) - 636.13;
+} 
+else {
+  aliquotaIR = (salarioLiquido * 0.275) - 869.36;
+}
+
+salarioLiquido = salarioBruto - aliquotaINSS - aliquotaIR;
+console.log ("O salário liquido é de: " + salarioLiquido);
