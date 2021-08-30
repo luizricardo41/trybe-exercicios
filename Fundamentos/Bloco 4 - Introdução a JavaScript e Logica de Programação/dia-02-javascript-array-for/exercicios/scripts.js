33de19b12cbf8d4aac195f6859826f678eea138f
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-30 08:39:18 -0300

    Resolução do exercicio 3 - Array e loop For

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-02-javascript-array-for/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-02-javascript-array-for/exercicios/scripts.js
@@ -9,10 +9,23 @@
 
 //Exercicio 2 - somar todos os valores contidos na array e imprimir o resultado.
 
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let sum = 0;
+//
+//for (let i = 0; i < numbers.length; i += 1){
+//  sum = numbers[i] + sum;
+//}
+//console.log (sum);
+
+//Exercicio 3 - Calcule e imprima a média aritmetica dos valores contidos no array.
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
+let media = 0;
 
 for (let i = 0; i < numbers.length; i += 1){
   sum = numbers[i] + sum;
 }
-console.log (sum);
+media = (sum / numbers.length);
+
+console.log (media);
