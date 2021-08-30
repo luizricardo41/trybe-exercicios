90bf5fa96686a17d20a0ec06f1fb2b239f618c8a
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-30 09:00:38 -0300

    Resolução do exercicio 5 - Array e loop For

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-02-javascript-array-for/exercicios/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-02-javascript-array-for/exercicios/scripts.js
@@ -32,17 +32,32 @@
 
 //Exercicio 4 - Verificar se o valor da média é maior ou menor que 20.
 
+//let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+//let sum = 0;
+//let media = 0;
+//
+//for (let i = 0; i < numbers.length; i += 1){
+//  sum = numbers[i] + sum;
+//}
+//media = (sum / numbers.length);
+//
+//if (media > 20){
+//  console.log ("Valor maior do que 20");
+//} else{
+//  console.log ("Valor menor ou igual a 20");
+//}
+
+//Exercicio 5 - Encontrar o maior valor da array e imprimi-lo.
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
-let media = 0;
+let valorMaior = 0;
+let valor = 0;
 
 for (let i = 0; i < numbers.length; i += 1){
-  sum = numbers[i] + sum;
+  valor = numbers[i];
+  
+  if(valor > 0 && valor >= valorMaior){
+    valorMaior = valor;
+  }
 }
-media = (sum / numbers.length);
-
-if (media > 20){
-  console.log ("Valor maior do que 20");
-} else{
-  console.log ("Valor menor ou igual a 20");
-}
+console.log(valorMaior);
