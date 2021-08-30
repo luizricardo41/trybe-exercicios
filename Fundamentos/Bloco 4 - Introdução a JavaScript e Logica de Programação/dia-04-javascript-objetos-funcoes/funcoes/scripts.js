4f7eab5020174cfc773dc9dfc8da215e4c830b5f
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-30 19:59:27 -0300

    Refatoração do exercicio 1

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/funcoes/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/funcoes/scripts.js
@@ -1,35 +1,48 @@
 // Exercicio 1 - Fazer 5 programas, um para cada operação aritmética:
 
-//const a = 32;
-//const b = 8;
-//
-////adição
-//let soma;
-//
-//soma = (a+b);
-//console.log(soma);
-//
-////subtração
-//let diferença;
-//
-//diferença = (a-b);
-//console.log(diferença);
-//
-////divisão
-//let quociente;
-//
-//quociente = (a/b);
-//console.log(quociente);
-//
-////multiplicação
-//let produto;
-//
-//produto = (a*b);
-//console.log(produto);
-//
-////modulo
+//FUNCTION DA SOMA;
+
+/*function sum(a, b) {
+  return a + b;
+}
+let result = sum (10, 30);
+console.log(result);*/
+
+//FUNCTION DA SUBTRAÇÃO;
+
+//function subtraction(a, b) {
+//  return a - b;
+//}
+//let result = subtraction (10, 30);
+//console.log(result);
+
+//FUNCTION DA DIVISÃO;
+
+//function division(a, b) {
+//  return b / a;
+//}
+//let result = division (10, 30);
+//console.log(result);
+
+//FUNCTION DA MULTIPLICAÇÃO;
+
+//function multiplication(a, b) {
+//  return a * b;
+//}
+//let result = multiplication (10, 30);
+//console.log(result);
+
+//FUNCTION DO MODULO;
+
+function module(a, b) {
+  return a % b;
+}
+let result = module (10, 3);
+console.log(result);
+
+//modulo
 //let resto;
-//
+
 //resto = (a%b);
 //console.log(resto);
 
@@ -208,43 +221,43 @@
 
 //Exercicio 11 - Programa para calcular o salario liquido de acordo com descontos dos impostos para cada faixa de salário.
 
-const salarioBruto = 10000.30;
-let salarioLiquido;
-let aliquotaINSS;
-let aliquotaIR;
-
-if (salarioBruto <= 1556.94) {
-  aliquotaINSS = salarioBruto * 0.08;
-  salarioLiquido = salarioBruto - aliquotaINSS;
-} 
-else if (salarioBruto <= 2594.92){
-  aliquotaINSS = salarioBruto * 0.09;
-  salarioLiquido = salarioBruto - aliquotaINSS;
-}
-else if (salarioBruto <= 5189.82){
-  aliquotaINSS = salarioBruto * 0.11;
-  salarioLiquido = salarioBruto - aliquotaINSS;
-}
-else {
-  aliquotaINSS = 570.88;
-  salarioLiquido = salarioBruto - aliquotaINSS;
-}
-
-if (salarioLiquido <= 1903.98) {
-  aliquotaIR = 0;
-}
-else if (salarioLiquido <= 2826.65) {
-  aliquotaIR = (salarioLiquido * 0.075) - 142.80;
-}
-else if (salarioLiquido <= 3751.05) {
-  aliquotaIR = (salarioLiquido * 0.15) - 354,80;
-}
-else if (salarioLiquido <= 4664.68) {
-  aliquotaIR = (salarioLiquido * 0.225) - 636.13;
-} 
-else {
-  aliquotaIR = (salarioLiquido * 0.275) - 869.36;
-}
-
-salarioLiquido = salarioBruto - aliquotaINSS - aliquotaIR;
-console.log ("O salário liquido é de: " + salarioLiquido);
+//const salarioBruto = 10000.30;
+//let salarioLiquido;
+//let aliquotaINSS;
+//let aliquotaIR;
+//
+//if (salarioBruto <= 1556.94) {
+//  aliquotaINSS = salarioBruto * 0.08;
+//  salarioLiquido = salarioBruto - aliquotaINSS;
+//} 
+//else if (salarioBruto <= 2594.92){
+//  aliquotaINSS = salarioBruto * 0.09;
+//  salarioLiquido = salarioBruto - aliquotaINSS;
+//}
+//else if (salarioBruto <= 5189.82){
+//  aliquotaINSS = salarioBruto * 0.11;
+//  salarioLiquido = salarioBruto - aliquotaINSS;
+//}
+//else {
+//  aliquotaINSS = 570.88;
+//  salarioLiquido = salarioBruto - aliquotaINSS;
+//}
+//
+//if (salarioLiquido <= 1903.98) {
+//  aliquotaIR = 0;
+//}
+//else if (salarioLiquido <= 2826.65) {
+//  aliquotaIR = (salarioLiquido * 0.075) - 142.80;
+//}
+//else if (salarioLiquido <= 3751.05) {
+//  aliquotaIR = (salarioLiquido * 0.15) - 354,80;
+//}
+//else if (salarioLiquido <= 4664.68) {
+//  aliquotaIR = (salarioLiquido * 0.225) - 636.13;
+//} 
+//else {
+//  aliquotaIR = (salarioLiquido * 0.275) - 869.36;
+//}
+//
+//salarioLiquido = salarioBruto - aliquotaINSS - aliquotaIR;
+//console.log ("O salário liquido é de: " + salarioLiquido);
