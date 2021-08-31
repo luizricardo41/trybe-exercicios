14266b144898aaae682adeb067370a22aa1554db
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-31 16:28:27 -0300

    Refatoração do exercicio 11

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/funcoes/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/funcoes/scripts.js
@@ -235,7 +235,7 @@ console.log(result);*/
 
 // FUNCTION PROGRAMA PARA CALCULAR O LUCRO DA VENDA DE UM PRODUTO;
 
-function calculaLucro (custo, venda) {
+/*function calculaLucro (custo, venda) {
 
     custoTotal = custo + (custo * 0.20);
 
@@ -248,62 +248,60 @@ function calculaLucro (custo, venda) {
 }
 
 let lucroTotal = calculaLucro(150, 320)
-console.log(lucroTotal);
+console.log(lucroTotal);*/
 
-
-//const custo = 130;
-//const venda = 320;
-//let lucro;
-//let custoTotal;
-//
-//custoTotal = custo + (custo * 0.20);
-//
-//if (custo <= 0 || venda <= 0) {
-//  console.log ("Os valores não podem ser iguais ou menores que zero!")
-//} else {
-//  lucro = (1000 * venda) - (1000 * custoTotal);
-//}
-//console.log ("O lucro será de: " + lucro);
 
 //Exercicio 11 - Programa para calcular o salario liquido de acordo com descontos dos impostos para cada faixa de salário.
 
-//const salarioBruto = 10000.30;
-//let salarioLiquido;
-//let aliquotaINSS;
-//let aliquotaIR;
-//
-//if (salarioBruto <= 1556.94) {
-//  aliquotaINSS = salarioBruto * 0.08;
-//  salarioLiquido = salarioBruto - aliquotaINSS;
-//} 
-//else if (salarioBruto <= 2594.92){
-//  aliquotaINSS = salarioBruto * 0.09;
-//  salarioLiquido = salarioBruto - aliquotaINSS;
-//}
-//else if (salarioBruto <= 5189.82){
-//  aliquotaINSS = salarioBruto * 0.11;
-//  salarioLiquido = salarioBruto - aliquotaINSS;
-//}
-//else {
-//  aliquotaINSS = 570.88;
-//  salarioLiquido = salarioBruto - aliquotaINSS;
-//}
-//
-//if (salarioLiquido <= 1903.98) {
-//  aliquotaIR = 0;
-//}
-//else if (salarioLiquido <= 2826.65) {
-//  aliquotaIR = (salarioLiquido * 0.075) - 142.80;
-//}
-//else if (salarioLiquido <= 3751.05) {
-//  aliquotaIR = (salarioLiquido * 0.15) - 354,80;
-//}
-//else if (salarioLiquido <= 4664.68) {
-//  aliquotaIR = (salarioLiquido * 0.225) - 636.13;
-//} 
-//else {
-//  aliquotaIR = (salarioLiquido * 0.275) - 869.36;
-//}
-//
-//salarioLiquido = salarioBruto - aliquotaINSS - aliquotaIR;
-//console.log ("O salário liquido é de: " + salarioLiquido);
+// FUNCTION PROGRAMA PARA CALCULAR SALARIO LIQUIDO DE ACORDO COM DESCONTOS DOS IMPOSTOS.
+
+function salarioDescontadoINSS (){
+
+  let salarioBruto = 3000;
+  let aliquotaINSS;
+  let descontoINSS;
+
+  if (salarioBruto <= 1556.94) {
+  aliquotaINSS = salarioBruto * 0.08;
+  descontoINSS = salarioBruto - aliquotaINSS;
+  } 
+  else if (salarioBruto <= 2594.92){
+    aliquotaINSS = salarioBruto * 0.09;
+    descontoINSS = salarioBruto - aliquotaINSS;
+  }
+  else if (salarioBruto <= 5189.82){
+    aliquotaINSS = salarioBruto * 0.11;
+    descontoINSS = salarioBruto - aliquotaINSS;
+  }
+  else {
+    aliquotaINSS = 570.88;
+    descontoINSS = salarioBruto - aliquotaINSS;
+  }
+
+  return descontoINSS;
+}
+
+function calculoIR () {
+  
+  let salarioDescontoINSS = salarioDescontadoINSS();
+
+  if (salarioDescontoINSS <= 1903.98) {
+    aliquotaIR = 0;
+  }
+  else if (salarioDescontoINSS <= 2826.65) {
+    aliquotaIR = (salarioDescontoINSS * 0.075) - 142.80;
+  }
+  else if (salarioDescontoINSS <= 3751.05) {
+    aliquotaIR = (salarioDescontoINSS * 0.15) - 354,80;
+  }
+  else if (salarioDescontoINSS <= 4664.68) {
+    aliquotaIR = (salarioDescontoINSS * 0.225) - 636.13;
+  } 
+  else {
+    aliquotaIR = (salarioDescontoINSS * 0.275) - 869.36;
+  }
+  return aliquotaIR;
+}
+
+let salarioLiquido = salarioDescontadoINSS () - calculoIR ();
+console.log ("O salário liquido é de: " + salarioLiquido);
