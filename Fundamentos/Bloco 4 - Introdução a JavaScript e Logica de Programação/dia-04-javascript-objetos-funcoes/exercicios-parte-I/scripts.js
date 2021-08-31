3d3672702a786f62377ce83121ef6d2781792062
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-31 16:44:03 -0300

    Resolução do exercicio 1 - Parte I

---
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-I/scripts.js
@@ -0,0 +1,9 @@
+// EXERCICIO 1 - IMPRIMIR NO CONSOLE UMA MENSAGEM DE BOAS VINDAS PARA A PERSONAGEM.
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log ("Bem vinda, " + info.personagem + ".");
