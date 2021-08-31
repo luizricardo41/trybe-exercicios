7dd9b05f8ff2d09c2f455cbb829d4380f8b54043
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-31 17:09:13 -0300

    Resolução do exercicio 5 - Parte I

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-I/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-I/scripts.js
@@ -36,7 +36,7 @@ for (let key in info){
 
 // EXERCICIO 4 - FAZER UM NOVO FOR/IN QUE MOSTRE OS VALORES DAS CHAVES
 
-let info = {
+/*let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
@@ -46,4 +46,25 @@ info['recorrente'] = 'sim';
 
 for (let key in info){
   console.log (info[key]);
+}*/
+
+// EXERCICIO 5 - DEFINIR UM NOVO OBJETO COM AS MESMAS CHAVES E IMPRIMIR AMBOS OS OBJETOS
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+info['recorrente'] = 'sim';
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: 'sim',
+};
+
+for (let key in info){
+  console.log (info[key] + " e " + info2[key]);
 }
