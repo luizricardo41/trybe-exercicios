42c9583f273b7420f0575c7aa654cb18767987c6
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-31 16:49:14 -0300

    Resolução do exercicio 2 - Parte I

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-I/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-I/scripts.js
@@ -1,9 +1,21 @@
 // EXERCICIO 1 - IMPRIMIR NO CONSOLE UMA MENSAGEM DE BOAS VINDAS PARA A PERSONAGEM.
 
+/*let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+console.log ("Bem vinda, " + info.personagem + ".");*/
+
+// EXERCICIO 2 - INSERIR NOVA PROPRIEDADE AO OBJETO
+
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
 
-console.log ("Bem vinda, " + info.personagem + ".");
+info['recorrente'] = 'sim';
+
+console.log (info);
