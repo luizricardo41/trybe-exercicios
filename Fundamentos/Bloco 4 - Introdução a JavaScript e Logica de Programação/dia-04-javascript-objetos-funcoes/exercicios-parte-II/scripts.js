1d1d09d2750581893b433f6b84960eef301e9bb7
Luiz Ricardo <luizricardo41@gmail.com>
2021-08-31 21:21:00 -0300

    Resolução do exercicio 2 - Parte II

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-II/scripts.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-04-javascript-objetos-funcoes/exercicios-parte-II/scripts.js
@@ -1,4 +1,6 @@
-function verificaPalindrome (word) {
+// EXERCICIO 1 - PARTE II - CRIAR UMA FUNÇÃO E RETORNAR TRUE SE FOR UM PALINDROMO OU FALSE SE NÃO FOR.
+
+/*function verificaPalindrome (word) {
 
   let aux = '';
   let answers = false;
@@ -13,4 +15,25 @@ function verificaPalindrome (word) {
 }
 
 let result = verificaPalindrome ('casa');
+console.log (result);*/
+
+// EXERCICIO 2 - PARTE II - CRIAR UMA FUNÇÃO QUE RECEBA UM ARRAY E RETORNE O INDICE DO MAIOR VALOR.
+
+function indexHigherValue (arrayValue) {
+
+  let value = 0;
+  let higherValue = 0;
+
+  for (let i = 0; i < arrayValue.length; i += 1){
+    value = arrayValue[i];
+
+    if (value >= higherValue){
+      higherValue = value;
+    }
+  }
+  return higherValue;
+}
+
+let arrayTest = [3, 106, 20, 1, 50, 67, 103, 89, 35];
+let result = indexHigherValue(arrayTest);
 console.log (result);
