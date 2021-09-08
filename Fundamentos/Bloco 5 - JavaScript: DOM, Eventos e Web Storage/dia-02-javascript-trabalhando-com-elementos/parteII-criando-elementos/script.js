577f7f031d69610891f529b05c066fb3b1e4570c
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-08 09:26:06 -0300

    Criando diretorio e arquivo dos exercicios Parte II

---
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia-02-javascript-trabalhando-com-elementos/parteII-criando-elementos/script.js
@@ -0,0 +1,3 @@
+const elemento = document.querySelector('#elementoOndeVoceEsta').parentNode;
+elemento.style.color = "red"
+document.getElementById('primeiroFilhoDoFilho').innerText = "Aqui está o primeiro filho do filho."
