a6448899940197f0a52688c282ed696170a550ca
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-08 18:24:27 -0300

    Resolução exercicio 1

---
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -0,0 +1,4 @@
+let bodyContent = document.querySelector('.content-body');
+let titleH1 = document.createElement('h1');
+titleH1.innerText = 'Exercício 5.2 - JavaScript DOM';
+bodyContent.appendChild(titleH1);
