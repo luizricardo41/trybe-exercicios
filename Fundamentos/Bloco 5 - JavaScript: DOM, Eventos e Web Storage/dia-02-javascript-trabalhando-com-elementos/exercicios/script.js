1231ee59b89d8e4b094831ef1536121cfb521e51
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-09 07:28:59 -0300

    Resolução exercicio 1 - Adicionando classe a Tag H1

--- Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
+++ Fundamentos/Bloco 5 - JavaScript: DOM, Eventos e Web Storage/dia-02-javascript-trabalhando-com-elementos/exercicios/script.js
@@ -1,5 +1,6 @@
 let bodyContent = document.querySelector('.content-body');
 let titleH1 = document.createElement('h1');
+titleH1.className = "title";
 titleH1.innerText = 'Exercício 5.2 - JavaScript DOM';
 bodyContent.appendChild(titleH1);
 
