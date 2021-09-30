01cc9f35bdabaf62d514855eb21f128f0b20f396
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-30 09:09:20 -0300

    Resoluçao do exercicio 3

--- Fundamentos/Bloco 7 - Introdução à Javascript ES6 e Testes Unitários/dia-03-primeiros-passos-Jest/exercicios/sum.test.js
+++ Fundamentos/Bloco 7 - Introdução à Javascript ES6 e Testes Unitários/dia-03-primeiros-passos-Jest/exercicios/sum.test.js
@@ -7,4 +7,7 @@ describe('function sum', () => {
   it ('sum of 0 and 0 equal 0', () => {
     expect(0).toBe(sum(0, 0));
   });
+  it ('error when string is sent', () => {
+    expect(() => { sum ('a')}).toThrow('parameters must be numbers');
+  });
 });
