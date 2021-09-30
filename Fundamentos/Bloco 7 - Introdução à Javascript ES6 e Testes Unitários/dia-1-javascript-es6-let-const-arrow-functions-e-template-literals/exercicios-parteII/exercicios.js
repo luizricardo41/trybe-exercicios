0f7be2b44c3e49d7fff0d2f11f0221f925edc0b8
Luiz Ricardo <luizricardo41@gmail.com>
2021-09-30 10:39:22 -0300

    Resoluçao do exercicio 2

--- Fundamentos/Bloco 7 - Introdução à Javascript ES6 e Testes Unitários/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parteII/exercicios.js
+++ Fundamentos/Bloco 7 - Introdução à Javascript ES6 e Testes Unitários/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios-parteII/exercicios.js
@@ -1,12 +1,25 @@
-const fatorial = (number) => {
-  if (typeof (number) === 'string' || number === undefined) {
-    return 'O parametro precisa ser um número!';
-  }
-  let produto = number;
-  for (index = number - 1; index > 0; index -= 1) {
-    produto = produto * index;
-  }
-  return produto;
-}
+// const fatorial = (number) => {
+//   if (typeof (number) === 'string' || number === undefined) {
+//     return 'O parametro precisa ser um número!';
+//   }
+//   let produto = number;
+//   for (index = number - 1; index > 0; index -= 1) {
+//     produto = produto * index;
+//   }
+//   return produto;
+// }
 
-console.log(fatorial(4));
+// console.log(fatorial(4));
+
+const longestWord = (sentence) => {
+  let words = sentence.split(' ');
+  let biggerWord = '';
+  for (index = 0; index < words.length; index += 1) {
+    if (words[index].length > 0 && words[index].length > biggerWord.length) {
+      biggerWord = words[index];
+    }
+  }
+  return biggerWord;
+}
+console.log(longestWord('Antônio foi no banheiro e não sabemos o que aconteceu'))
+ 
