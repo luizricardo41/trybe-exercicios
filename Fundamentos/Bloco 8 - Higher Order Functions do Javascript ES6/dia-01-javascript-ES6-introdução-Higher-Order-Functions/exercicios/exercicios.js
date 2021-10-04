6a61cc72c5b787ed1139bdb181e747fcaaddd6e0
Luiz Ricardo <luizricardo41@gmail.com>
2021-10-04 15:59:33 -0300

    Resoluçao do exercicio 1

--- Fundamentos/Bloco 8 - Higher Order Functions do Javascript ES6/dia-01-javascript-ES6-introdução-Higher-Order-Functions/exercicios/exercicios.js
+++ Fundamentos/Bloco 8 - Higher Order Functions do Javascript ES6/dia-01-javascript-ES6-introdução-Higher-Order-Functions/exercicios/exercicios.js
@@ -1,8 +1,19 @@
-const newEmployees = () => {
+const newPerson = (name) => {
+  let newName = name.split(' ').join('').toLowerCase();
+  const newObject = {
+    name,
+    email: `${newName}@trybe.com`
+  }
+  return newObject;
+}
+
+const newEmployees = (action) => {
   const employees = {
-    id1: '', // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: '', // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: '', // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: action('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: action('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: action('Carlos Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
+
+console.log(newEmployees(newPerson));
