c179c1c6764c35851d73a3ed262fd8e508ca3730
Luiz Ricardo <luizricardo41@gmail.com>
2021-11-05 11:09:43 -0300

    Criaçao de formulario

--- Fundamentos/Bloco 11 - Componentes com Estado, Eventos e Formulários React/dia-02-formulários-no-react/my-form-2.0/src/App.js
+++ Fundamentos/Bloco 11 - Componentes com Estado, Eventos e Formulários React/dia-02-formulários-no-react/my-form-2.0/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
 
 function App() {
   return (
-    <div className="App">
- 
+    <div>
+      <h2>Formulário de Cadastro</h2>
+      <Form />
     </div>
   );
 }
