4de9990ea094240cac44d8914d115f0ea2377c19
Luiz Ricardo <luizricardo41@gmail.com>
2021-12-13 19:42:24 -0300

    Resolucao exercicio 4

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicios/script.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicios/script.js
@@ -21,12 +21,31 @@
 
 // Exercicio 3 
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
-let value = '';
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let value = '';
 
-for (let i = 0; i < array.length; i += 1) {
-  if (value.length < array[i].length) {
-    value = array[i];
+// for (let i = 0; i < array.length; i += 1) {
+//   if (value.length < array[i].length) {
+//     value = array[i];
+//   }
+// }
+// console.log(value);
+
+
+// Exercicio 4
+
+let array = [];
+const value = 1000;
+let result = 0;
+
+for (let verifyNum = 2; verifyNum < value; verifyNum += 1) {
+  let count = 1;
+  for (let i = 2; i <= verifyNum; i += 1) {
+    if (verifyNum % i === 0) count += 1;
   }
+  if (count === 2) {
+    array.push(verifyNum);
+  }
+  result = array[array.length-1]
 }
-console.log(value);
+console.log(result);
