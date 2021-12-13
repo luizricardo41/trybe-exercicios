9da1acce8de5667487d8e6e34927dcd93bbe283c
Luiz Ricardo <luizricardo41@gmail.com>
2021-12-13 17:54:05 -0300

    Resolucao exercicio 2

--- Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicios/script.js
+++ Fundamentos/Bloco 4 - Introdução a JavaScript e Logica de Programação/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/exercicios/script.js
@@ -1,9 +1,21 @@
 // Exercicio 1
 
-let num = 10;
-let result = num;
+// let num = 10;
+// let result = num;
 
-for (let i = 1; i < num; i += 1) {
-  result = result * (num - i);
+// for (let i = 1; i < num; i += 1) {
+//   result = result * (num - i);
+// }
+// console.log(result);
+
+
+// Exercicio 2
+
+let word = 'banana';
+let newWord = '';
+
+for (let i = 1; i <= word.length; i += 1) {
+  newWord += word[word.length - i];
 }
-console.log(result);
+console.log(newWord);
+
