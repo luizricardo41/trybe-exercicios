c9a677f44bb3077abb553be5dd29bd6d08e7b40b
Luiz Ricardo <luizricardo41@gmail.com>
2022-01-07 19:55:30 -0300

    Implementado alguns testes

--- Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.js
+++ Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import ButtonClicks from './components/ButtonClicks';
 import NumberClicks from './components/NumberClicks';
-import store from './redux/store';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Provider store={store}>
-          <ButtonClicks />
-          <NumberClicks />
-        </Provider>
+        <ButtonClicks />
+        <NumberClicks />
       </div>
     );
   }
--- Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.test.js
+++ Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Provider } from 'react-redux';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { combineReducers, createStore } from 'redux';
+import clickReducer from './redux/reducers';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const renderWithRedux = (
+  component,
+  { initialState, store = createStore(combineReducers({ clickReducer }), initialState) } = {}
+) => {
+    return {
+      ...render(<Provider store={store}>{component}</Provider>),
+      store,
+    }
+  }
+
+
+describe('testing clicks', () => {
+  beforeEach(cleanup);
+  test('the page should has a button and a text 0', () => {
+    const { queryByText } = renderWithRedux(<App />);
+    const buttonAdicionar = queryByText('Clique aqui');
+
+    expect(buttonAdicionar).toBeInTheDocument();
+    expect(queryByText('0')).toBeInTheDocument();
+  })
+  const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 }}});
+  
+  test('a click in a button should increment the value of clicks', () => {
+
+    expect(queryByText('5')).toBeInTheDocument();
+  });
 });
