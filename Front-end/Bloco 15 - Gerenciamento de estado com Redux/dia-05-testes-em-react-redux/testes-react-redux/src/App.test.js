334e1aaabfa0ac576d421476e3c52eadd88fc926
Luiz Ricardo <luizricardo41@gmail.com>
2022-01-08 15:57:24 -0300

    Criados testes solicitados

--- Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.test.js
+++ Front-end/Bloco 15 - Gerenciamento de estado com Redux/dia-05-testes-em-react-redux/testes-react-redux/src/App.test.js
@@ -4,6 +4,7 @@ import { render, cleanup } from '@testing-library/react';
 import App from './App';
 import { combineReducers, createStore } from 'redux';
 import clickReducer from './redux/reducers';
+import userEvent from '@testing-library/user-event';
 
 const renderWithRedux = (
   component,
@@ -13,7 +14,6 @@ const renderWithRedux = (
       store,
     }
   }
-
 
 describe('testing clicks', () => {
   beforeEach(cleanup);
@@ -25,12 +25,25 @@ describe('testing clicks', () => {
     expect(queryByText('0')).toBeInTheDocument();
   })
 
-  
-  
-  test('a click in a button should increment the value of clicks', () => {
-
+  test('the value of clicks on the screen must be equal to the amount of clicks of the initial state', () => {
     const { queryByText } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 5 } } });
 
     expect(queryByText('5')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks', () => {
+    const { queryByText, getByRole } = renderWithRedux(<App />);
+
+    userEvent.click(getByRole('button', { name: /clique aqui/i }));
+
+    expect(queryByText('1')).toBeInTheDocument();
+  });
+
+  test('a click in a button should increment the value of clicks of the initial state', () => {
+    const { queryByText, getByRole } = renderWithRedux(<App />, { initialState: { clickReducer: { counter: 10 } } });
+
+    userEvent.click(getByRole('button', { name: /clique aqui/i }));
+
+    expect(queryByText('11')).toBeInTheDocument();
   });
 });
