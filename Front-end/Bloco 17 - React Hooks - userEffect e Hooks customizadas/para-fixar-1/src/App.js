cf07076a44969bc59e12a4bcb76b01551f2591f0
Luiz Ricardo <luizricardo41@gmail.com>
2022-01-25 14:19:33 -0300

    Testando implementaçao

--- Front-end/Bloco 17 - React Hooks - userEffect e Hooks customizadas/para-fixar-1/src/App.js
+++ Front-end/Bloco 17 - React Hooks - userEffect e Hooks customizadas/para-fixar-1/src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [num, setNum] = useState(0);
+  const [msg, setMsg] = useState('');
+  let [timer, setTimer] = useState(10);
+  const ramdomNum = () => {
+    const num = Math.floor(Math.random() * (100)) + 1;
+    setNum(num);
+    if (num % 3 === 0 || num % 5 === 0) {
+      setMsg('Acerto')
+    } else {
+      setMsg('');
+    }
+  }
+
+  const playTimer = () => {
+    if (timer > 0) {
+      setTimer(timer => timer - 1)
+    } else {
+      setTimer(10);
+    }
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      playTimer()
+    }, 1000);
+    return () => {
+      clearInterval(interval);
+    }
+  })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      ramdomNum()
+    }, 10000);
+    return () => {
+      clearInterval(interval);
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {timer} <br/>
+      {' '}
+      {num} <br/>
+      {' '}
+      {msg !== '' && msg}
     </div>
   );
 }
