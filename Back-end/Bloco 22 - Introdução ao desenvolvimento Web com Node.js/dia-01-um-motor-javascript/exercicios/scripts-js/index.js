8dd72574f33ceebbdc59e001d3a7c17115625b42
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-15 15:13:05 -0300

    fix: Modificação de arquivos index.js e package.json
    index.js - Implementada função para correto funcionamento do script;
    package.json - Mudança de chave dos scripts para utilizar o 'npm start';

--- Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/dia-01-um-motor-javascript/exercicios/scripts-js/index.js
+++ Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/dia-01-um-motor-javascript/exercicios/scripts-js/index.js
@@ -1,8 +1,27 @@
 const calcImc = require('./servicesIMC/calc_imc');
 const calc_velocidade = require('./servicesSpeed/calc_velocidade');
+const sortNumber = require('./servicesSorteio/realiza_sorteio');
+const readline = require('readline-sync');
 
 function main() {
-  calc_velocidade();
+  const options = {
+    1: 'Calcular IMC.',
+    2: 'Calcular Velocidade.',
+    3: 'Sorteio de 1 a 10'
+  }
+  
+  console.table(options);
+  const option = readline.questionInt('Selecione uma das opções acima: ');
+
+  if (option === 1) {
+    calcImc();
+  }
+  if (option === 2) {
+    calc_velocidade();
+  }
+  if (option === 3) {
+    sortNumber();
+  }
 }
 
 main();
