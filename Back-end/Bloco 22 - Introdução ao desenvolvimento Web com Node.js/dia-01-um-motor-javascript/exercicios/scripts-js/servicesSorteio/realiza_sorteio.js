1a603d97e839ab0741c0afbcd427ceae064ce2a9
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-15 14:55:52 -0300

    feat: Arquivo e função para checar o realiza_sorteio
    Foram criados arquivo e a função para verificar se o número digitado foi acertado;

---
+++ Back-end/Bloco 22 - Introdução ao desenvolvimento Web com Node.js/dia-01-um-motor-javascript/exercicios/scripts-js/servicesSorteio/realiza_sorteio.js
@@ -0,0 +1,14 @@
+const dataSort = require('./data_sorteio');
+
+const checkNumber = () => {
+  const { insertNumber, data } = dataSort;
+  insertNumber();
+
+  if (data.sortNumber === data.choseNumber) {
+    console.log('Parabéns, número correto!')
+  } else {
+    console.log(`Opa, não foi dessa vez. O numero sorteado era ${sortNumber}!`);
+  }
+}
+
+module.exports = checkNumber;
