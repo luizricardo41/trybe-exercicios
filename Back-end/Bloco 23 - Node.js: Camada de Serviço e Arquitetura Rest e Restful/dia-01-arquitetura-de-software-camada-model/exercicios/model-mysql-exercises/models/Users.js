bc6ebca240bb507e6c2dc8af1bda819313173b99
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-25 09:07:36 -0300

    feat: Criado arquivo Users, responsável pela comunicação com o DB;
    Implementada funções de validação e adição de novo user;

--- Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-01-arquitetura-de-software-camada-model/exercicios/model-mysql-exercises/models/Users.js
+++ Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-01-arquitetura-de-software-camada-model/exercicios/model-mysql-exercises/models/Users.js
@@ -1,20 +1,48 @@
 const connection = require("./connection");
 
 const validation = (first_name, last_name, email, password) => {
-  if (!first_name || typeof (first_name) !== 'string') return false;
-  if (!last_name || typeof (last_name) !== 'string') return false;
-  if (!email || typeof (email) !== 'string') return false;
-  if (!password || typeof (password) !== 'string') return false;
+  if (!first_name || typeof (first_name) !== 'string') {
+    return {
+      error: true,
+      message: `O campo "first_name" deve existir e ser uma string`
+    }
+  };
+  if (!last_name || typeof (last_name) !== 'string') {
+    return {
+      error: true,
+      message: `O campo "last_name" deve existir e ser uma string`
+    }
+  };
+  
+  if (!email || typeof (email) !== 'string') {
+    return {
+      error: true,
+      message: `O campo "email" deve existir e ser uma string`
+    };
+  };
+  if (!password || typeof (password) !== 'string' || password.length < 6) {
+    return {
+      error: true,
+      message: `O campo "password" ser uma string e ter pelo menos 6 caracteres`
+    };
+  };
   
   return true;
 }
 
-const createUser = async (first_name, last_name, email, password) => {
-  connection.execute(
+const createUser = async ({ first_name, last_name, email, password }) => {
+  const [{ insertId }] = await connection.execute(
     `INSERT INTO data_users.user 
     (first_name, last_name, email, password) VALUES (?, ?, ?, ?)`,
     [first_name, last_name, email, password]
   );
+  return {
+    id: insertId,
+    first_name,
+    last_name,
+    email,
+    password
+  };
 }
 
 module.exports = {
