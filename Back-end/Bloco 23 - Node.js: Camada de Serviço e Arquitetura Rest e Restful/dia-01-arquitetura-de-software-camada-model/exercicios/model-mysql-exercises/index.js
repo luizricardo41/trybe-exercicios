6cdee8814a4e93f439914bc914ba4401bdfaed27
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-25 09:04:17 -0300

    feat: Criação de arquivo principal index;
    Criado arquivo e implementada rota de adição de novo user;

---
+++ Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-01-arquitetura-de-software-camada-model/exercicios/model-mysql-exercises/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const Users = require('./models/Users');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const port = 3000;
+
+app.post('/user', async (req, res) => {
+  const { first_name, last_name, email, password } = req.body;
+
+  const isValid = Users.validation(first_name, last_name, email, password);
+
+  if (isValid === true) {
+    const userAdd = await Users
+      .createUser({ first_name, last_name, email, password });
+    return res.status(201).json(userAdd);
+  }
+  return res.status(400).json(isValid);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`))
