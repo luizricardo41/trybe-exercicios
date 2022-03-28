5d29273a704198f709128a0dcf300a6be2ecfbbc
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-28 10:10:27 -0300

    feat: implementada configuração para conexão com DB;
    Implementada a função connection para conexão entre aplicação e banco de dados;

---
+++ Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-02-arquitetura-de-software-camada-de-controller-service/exercicios/model/connection.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2/promise');
+
+const connection = mysql.createPool({
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  database: process.env.MYSQL_DB_NAME
+});
+
+module.exports = connection;
