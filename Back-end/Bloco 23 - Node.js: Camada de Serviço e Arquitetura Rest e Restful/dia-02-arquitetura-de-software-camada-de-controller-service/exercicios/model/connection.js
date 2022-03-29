380379d46980dae8346fe7cc3dedda37781a1312
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-29 09:06:11 -0300

    feat: implementada configuração connection;
    A configuração connection foi implementada para conexão da aplicação com o banco de dados MySQL;

--- Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-02-arquitetura-de-software-camada-de-controller-service/exercicios/model/connection.js
+++ Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-02-arquitetura-de-software-camada-de-controller-service/exercicios/model/connection.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2/promise');
 
+require('dotenv').config();
+
 const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
