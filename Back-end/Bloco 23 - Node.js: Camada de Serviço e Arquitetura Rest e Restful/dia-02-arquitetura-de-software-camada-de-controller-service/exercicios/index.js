4d508260044b90653d69dee65723b2dbd824061c
Luiz Ricardo <luizricardo41@gmail.com>
2022-03-29 08:56:56 -0300

    fix: implementada rota de busca por ceps;
    Criada a rota de busca por cep;

--- Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-02-arquitetura-de-software-camada-de-controller-service/exercicios/index.js
+++ Back-end/Bloco 23 - Node.js: Camada de Serviço e Arquitetura Rest e Restful/dia-02-arquitetura-de-software-camada-de-controller-service/exercicios/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const Controller = require('./controller/cepController');
+
 const app = express();
 
 app.use(bodyParser.json());
@@ -10,5 +12,7 @@ const PORT = 3000;
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: "pong!" });
 });
+
+app.get('/cep/:cep', Controller.receiveCep);
 
 app.listen(PORT, () => console.log('App listening on port: ' + PORT));
