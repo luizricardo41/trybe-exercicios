133fb5594b9ba254a51191b1ae771a48e57766af
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-08 14:19:43 -0300

    feat: configuração da API e criação da rota GET/books;

---
+++ Back-end/Bloco 24 - Node.js: ORM e Autenticação/dia-01-ORM-Interface-da-aplicação-com-o-banco-de-dados/exercicios/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const BooksController = require('./controller/booksController')
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.get('/books', BooksController.getAll);
+
+app.listen(3000, () => console.log('Listening on port 3000'));
