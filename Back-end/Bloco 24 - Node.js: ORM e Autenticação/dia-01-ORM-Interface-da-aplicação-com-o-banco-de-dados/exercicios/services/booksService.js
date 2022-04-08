a7bf3c9ebb8631c246bbefa0c13c7f95aaa9afa9
Luiz Ricardo <luizricardo41@gmail.com>
2022-04-08 19:20:06 -0300

    feat: função para busca de livro por id. (Service);

--- Back-end/Bloco 24 - Node.js: ORM e Autenticação/dia-01-ORM-Interface-da-aplicação-com-o-banco-de-dados/exercicios/services/booksService.js
+++ Back-end/Bloco 24 - Node.js: ORM e Autenticação/dia-01-ORM-Interface-da-aplicação-com-o-banco-de-dados/exercicios/services/booksService.js
@@ -1,13 +1,18 @@
 const { Book } = require('../models');
 
 const getAll = async () => {
-  const books = await Book.findAll({
-    attributes: ['id', 'title', 'author', 'pageQuantity', 'createdAt']
-  });
+  const books = await Book.findAll();
 
   return books;
 };
 
+const getById = async (id) => {
+  const book = await Book.findByPk(id);
+
+  return book;
+};
+
 module.exports = {
   getAll,
+  getById,
 };
